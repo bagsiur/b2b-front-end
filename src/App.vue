db44745fe093f116c26328fdd92123a8ed80edbb
Radoslaw Andraszyk <radoslaw.andraszyk@gmail.com>
2021-07-21 11:51:11 +0300

    All changes

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-    <router-view/>
+    <div id="main">
+        <router-view/>
+    </div>
 </template>
+
+<style>
+    @import './assets/coreui.css';
+    @import '~bootstrap/dist/css/bootstrap.min.css';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
+import { setupI18n } from './i18n.js'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas);
+//https://www.youtube.com/watch?v=RIjIkMcdoII
+createApp(App)
+    .use(setupI18n)
+    .use(store)
+    .use(router)
+    .component("fa", FontAwesomeIcon)
+    .mount('#app')
